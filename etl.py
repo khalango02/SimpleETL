850843eb257f21b5270b1e386e30f062eb64d51b
Clesley Oliveira <cl.oliveira@MacBook-Air-Clesley.local>
2024-02-29 18:38:59 -0300

    V!

---
+++ etl.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# read CSV file
+column_names = ['person','year', 'company']
+
+df = pd.read_csv('/Users/cl.oliveira/Documents/SimpleETL/test.csv', header = None, names = column_names)
+print(df)
+
+df = pd.read_csv('/Users/cl.oliveira/Documents/SimpleETL/test.csv', header = 0)
+print(df)
+
+
+engine = create_engine('mysql://admin:admin@127.0.0.1:3306/db')
+with engine.connect() as conn, conn.begin():
+    df.to_sql('teste', conn, if_exists='append', index=False)
