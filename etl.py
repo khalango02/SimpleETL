8b4558ac67ae7ffc14ba5e5ca23ae2fa649b9b43
Clesley Oliveira <cl.oliveira@MacBook-Air-Clesley.local>
2024-02-29 18:40:37 -0300

    V1

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # read CSV file
-column_names = ['person','year', 'company']
+column_names = ['name','birth', 'company']
 
 df = pd.read_csv('/Users/cl.oliveira/Documents/SimpleETL/test.csv', header = None, names = column_names)
 print(df)
@@ -13,4 +13,4 @@ print(df)
 
 engine = create_engine('mysql://admin:admin@127.0.0.1:3306/db')
 with engine.connect() as conn, conn.begin():
-    df.to_sql('teste', conn, if_exists='append', index=False)
+    df.to_sql('user', conn, if_exists='append', index=False)
